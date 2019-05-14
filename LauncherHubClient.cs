00536da39554657ba17de9ed0d6e24f545e4f492
alexalok <contact@alexalok.me>
2019-05-14 23:19:48 +0300

    Initial commit

---
+++ LauncherHubClient.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace SignalR_Test
+{
+    public class LauncherHubClient
+    {
+        const int MaxSendRetryCount = 5;
+        readonly HubConnection _connection;
+        readonly string _url;
+
+        public LauncherHubClient()
+        {
+            _url = "https://account.cryproxy.ru/LaunchersHub";
+            _connection = new HubConnectionBuilder().
+                WithUrl(_url).
+                Build();
+            _connection.Closed += _connection_Closed;
+        }
+
+        public void Start()
+        {
+            _ = Connect();
+        }
+
+        async Task Connect()
+        {
+            Console.WriteLine("Connecting to SignalR hub");
+            await _connection.StartAsync();
+            Console.WriteLine("Connected to hub");
+        }
+
+        async Task _connection_Closed(Exception ex)
+        {
+            Console.WriteLine("Hub closed: " + ex);
+
+            //get exception thrown
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            await Reconnect();
+        }
+
+        async Task Reconnect()
+        {
+            await Task.Delay(TimeSpan.FromMinutes(1));
+            try
+            {
+                await Connect();
+            }
+            catch
+            {
+                await Reconnect();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+            var hubClient = new LauncherHubClient();
+            hubClient.Start();
+            Console.ReadLine();
+        }
+
+        static void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            Console.WriteLine("TaskScheduler_UnobservedTaskException: " + e.Exception);
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Console.WriteLine("CurrentDomain_UnhandledException: " + e.ExceptionObject);
+        }
+    }
+}
